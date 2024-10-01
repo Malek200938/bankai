81c30d98287768722255114c9c9d8b91dd519424
Malek200938 <malekokamoka1990@gmail.com>
2024-10-01 17:19:26 +0300

    Delete task shop directory

--- task shop/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-const user = document.getElementById("name")
-const html = document.getElementById("html")
-const css = document.getElementById("css")
-const js = document.getElementById("js")
-const mysql = document.getElementById("mysql")
-const btn = document.getElementById("btn")
-
-
