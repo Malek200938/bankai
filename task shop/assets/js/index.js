3b6ea426dd7a6b8a0a507c8abf34bca5fd015f23
Malek200938 <malekokamoka1990@gmail.com>
2024-10-01 17:12:58 +0300

    Add files via upload

---
+++ task shop/assets/js/index.js
@@ -0,0 +1,8 @@
+const user = document.getElementById("name")
+const html = document.getElementById("html")
+const css = document.getElementById("css")
+const js = document.getElementById("js")
+const mysql = document.getElementById("mysql")
+const btn = document.getElementById("btn")
+
+
